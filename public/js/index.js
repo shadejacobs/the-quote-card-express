5251adac2343df3c4cbb507723854c6cf89826c4
shadejacobs <101609838+shadejacobs@users.noreply.github.com>
2025-06-23 18:52:48 -0400

    Add path correctly

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,19 @@
 console.log("Quote Card Loaded");
+
+
+async function getRandomImage() {
+    const client_id = "YOUR_ACCESS_KEY";
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
+
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getRandomImage();
