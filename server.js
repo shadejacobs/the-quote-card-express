bf8c3c57996ec25f657bd731526165bda2fbcda9
shadejacobs <101609838+shadejacobs@users.noreply.github.com>
2025-06-23 20:14:15 -0400

    Fix server.js and add dev script

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-"use strict";
-
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = 8080;
-app.use(express.static("./public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.listen(port, () => {
-    console.log(`Server is running http://localhost:${port}`);
-    console.log("Press Ctrl+C to end this process.");
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
-"scripts": {
-  "dev": "node server.js",
-  ...
-}
