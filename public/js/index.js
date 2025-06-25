7f979a76ef6f34e3e0bfeba09dc2718a5478b191
shadejacobs <101609838+shadejacobs@users.noreply.github.com>
2025-06-24 20:43:06 -0400

    Part 3 on index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,35 @@
-console.log("Quote Card Loaded");
+"use strict"
 
+const elements = {
+    quote: document.getElementById("quote"),
+    author: document.getElementById("author"),
+};
+const quotes = [
+    {
+        quote: "Be the change that you wish to see in the world.",
+        author: "Mahatma Gandi",
+    },
 
-async function getRandomImage() {
-    const client_id = "YOUR_ACCESS_KEY";
-    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
-    try {
-        const response = await fetch(endpoint);
-        const returnedData = await response.json();
-        const receivedPhotoUrl = returnedData.urls.regular;
+    {
+        quote: "The only thing we have to fear is fear itself.",
+        author: "Franklin D. Roosevelt, 1933 Inaugural Address",
+    },
 
-        const imgDiv = document.querySelector(".background-img");
-        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
-    } catch (error) {
-        console.error(error);
+    {
+        quote: "The Internet is the first thing that humanity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
+        author: "Eric Schmidt",
     }
+]
+function loopThroughQuotes() {
+    let quoteIndex = 0;
+    setInterval(() => {
+        if (quoteIndex < quotes.length) {
+            elements.quote.textContent = quotes[quoteIndex].quote;
+            elements.author.textContent = quotes[quoteIndex].author;
+            quoteIndex++;
+        } else {
+            quoteIndex = 0;
+        }
+    }, 3000);
 }
-
-getRandomImage();
+setTimeout(loopThroughQuotes, 3000);
