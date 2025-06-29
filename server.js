1b445f5ef904fae7a730efb3c03003e131c6ce61
shadejacobs <101609838+shadejacobs@users.noreply.github.com>
2025-06-29 17:57:01 -0400

    Update Q3

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+require("dotenv").config();
+const cors = require("cors");
+
+app.use(cors());
+
+async function getRandomImage () {
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${process.env.CLIENT_ID}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
+
+        return receivedPhotoUrl;
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 app.use(express.static('public'));
 
-app.use("/api/v1/getRandomImage", (request, response) => {
+app.use("/api/v1/getRandomImage", async (request, response) => {
     response.status(200).json({
         status: 200,
         data: await getRandomImage(),
@@ -16,6 +33,5 @@ app.listen(PORT, () => {
 });
 
 require("dotenv").config();
-const cors = require("cors");
 
 app.use(cors());
